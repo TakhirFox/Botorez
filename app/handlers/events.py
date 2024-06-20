a7c3e7c08b504ad606f641cc8c65b87dbd37e168
winzero <iamtheorangefox@gmail.com>
2024-06-21 00:47:18 +0700

    New

---
+++ app/handlers/events.py
@@ -0,0 +1,10 @@
+from app.settings import bot, secrets
+from app import views
+
+
+async def start_bot():
+    await bot.send_message(secrets.admin_id, views.start_bot_message())
+
+
+async def stop_bot():
+    await bot.send_message(secrets.admin_id, views.stop_bot_message())
